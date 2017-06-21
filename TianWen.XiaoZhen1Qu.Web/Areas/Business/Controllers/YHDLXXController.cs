b3d0c8e64605bb4181f4126a436a51582c2d4209
rebirth033 <980381266@qq.com>
2017-06-21 21:23:57 +0800

    first commit

---
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Controllers/YHDLXXController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web.Mvc;
+using System.Xml;
+using CommonClassLib.Helper;
+using TianWen.XiaoZhen1Qu.Entities.Models;
+using TianWen.XiaoZhen1Qu.Web.Areas.Business.Common;
+using TianWen.XiaoZhen1Qu.Interface;
+
+namespace TianWen.XiaoZhen1Qu.Web.Areas.Business.Controllers
+{
+    public class YHDLXXController : BaseController
+    {
+        public IYHJBXXBLL YHJBXXBLL { get; set; }
+
+        public ActionResult YHDLXX()
+        {
+            return View();
+        }
+    }
+}
