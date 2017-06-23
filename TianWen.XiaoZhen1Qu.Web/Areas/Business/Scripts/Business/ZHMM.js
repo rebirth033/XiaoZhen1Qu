470c78aa7b8dd12fc48fa893edf60c2f7135159a
rebirth033 <980381266@qq.com>
2017-06-23 22:23:45 +0800

    添加找回密码

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/ZHMM.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/ZHMM.js
@@ -1,6 +1,9 @@
-﻿$(document).ready(function () {
+﻿var count = 60;
+
+$(document).ready(function () {
     $("#YHM").bind("blur", YHMCheck);
-    $("#TXYZM").bind("blur", ValidateCheckCode);
+    $("#TXYZM").bind("blur", TXYZMCheck);
+    $("#btnHQYZM").bind("click", GetCheckCode);
     $("#btnFirst").bind("click", QRZH);
     $("#btnSecond").bind("click", YZZH);
     $("#spanQRZH").css("color", "#5bc0de");
@@ -47,17 +50,52 @@ function QRZH() {
 }
 
 function YZZH() {
-    $("#spanYZZH").css("color", "#cccccc");
-    $("#emYZZH").css("background", "#cccccc");
-    $("#spanCZMM").css("color", "#5bc0de");
-    $("#emCZMM").css("background", "#5bc0de");
-    $("#divSecond").css("display", "none");
-    $("#divThird").css("display", "block");
+    if (!YZZHValidate()) return;
+    $.ajax({
+        type: "POST",
+        url: getRootPath() + "/Business/ZHMM/YZZH",
+        dataType: "json",
+        data:
+        {
+            SJ: $("#SJ").val(),
+            YZM: $("#YZM").val(),
+        },
+        success: function (xml) {
+            if (xml.Result === 1) {
+                $("#spanYZZH").css("color", "#cccccc");
+                $("#emYZZH").css("background", "#cccccc");
+                $("#spanCZMM").css("color", "#5bc0de");
+                $("#emCZMM").css("background", "#5bc0de");
+                $("#divSecond").css("display", "none");
+                $("#divThird").css("display", "block");
+            } else {
+                if (xml.Type === 1) {
+                    $("#YZM").css("border-color", "#F2272D");
+                    $("#YZMInfo").css("color", "#F2272D");
+                    $("#YZMInfo").html(xml.Message);
+                }
+                if (xml.Type === 2) {
+                    $("#SJ").css("border-color", "#F2272D");
+                    $("#SJInfo").css("color", "#F2272D");
+                    $("#SJInfo").html(xml.Message);
+                }
+            }
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) { //有错误时的回调函数
+            _masker.CloseMasker(false, errorThrown);
+        }
+    });
 }
 
 function QRZHValidate() {
     if (!YHMCheck()) return false;
-    if (!ValidateCheckCode()) return false;
+    if (!TXYZMCheck()) return false;
+    return true;
+}
+
+function YZZHValidate() {
+    if (!SJCheck()) return false;
+    if (!YZMCheck()) return false;
     return true;
 }
 
@@ -75,7 +113,7 @@ function YHMCheck() {
     }
 }
 
-function ValidateCheckCode() {
+function TXYZMCheck() {
     if ($("#TXYZM").val().length === 0) {
         $("#TXYZM").css("border-color", "#F2272D");
         $("#TXYZMInfo").css("color", "#F2272D");
@@ -92,5 +130,140 @@ function ValidateCheckCode() {
         $("#TXYZM").css("border-color", "#999");
         $("#TXYZMInfo").html('');
         return true;
+    }
+}
+
+function SJCheck() {
+    if (!ValidateCellPhone($("#SJ").val())) {
+        $("#SJ").css("border-color", "#F2272D");
+        $("#SJInfo").css("color", "#F2272D");
+        $("#SJInfo").html("手机号码格式不正确");
+        return false;
+    }
+    else if ($("#SJ").val().length === 0) {
+        $("#SJ").css("border-color", "#F2272D");
+        $("#SJInfo").css("color", "#F2272D");
+        $("#SJInfo").html("请输入手机号");
+        return false;
+    }
+    else {
+        $("#SJ").css("border-color", "#999");
+        $("#SJInfo").html('');
+        return true;
+    }
+}
+
+function YZMCheck() {
+    if ($("#YZM").val().length === 0) {
+        $("#YZM").css("border-color", "#F2272D");
+        $("#YZMInfo").css("color", "#F2272D");
+        $("#YZMInfo").html("请输入手机验证码");
+        return false;
+    }
+    if (!/^[0-9]{6}$/.test($("#YZM").val())) {
+        $("#YZM").css("border-color", "#F2272D");
+        $("#YZMInfo").css("color", "#F2272D");
+        $("#YZMInfo").html("手机验证码输入格式有误");
+        return false;
+    }
+    else {
+        $("#YZM").css("border-color", "#999");
+        $("#YZMInfo").html('');
+        return true;
+    }
+}
+
+function MMCheck() {
+    if (($("#MM").val().length < 6 && $("#MM").val().length > 0) || $("#MM").val().length > 20) {
+        $("#MM").css("border-color", "#F2272D");
+        $("#MMInfo").css("color", "#F2272D");
+        $("#MMInfo").html("密码为6-20个字符，请修改");
+        return false;
+    }
+    if ($("#MM").val().length === 0) {
+        $("#MM").css("border-color", "#F2272D");
+        $("#MMInfo").css("color", "#F2272D");
+        $("#MMInfo").html("请输入密码");
+        return false;
+    }
+    if ($("#MM").val().split(' ').length > 1) {
+        $("#MM").css("border-color", "#F2272D");
+        $("#MMInfo").css("color", "#F2272D");
+        $("#MMInfo").html("密码不能包含空格，请修改");
+        return false;
+    }
+    if (!/^(?![^a-zA-Z]+$)(?!\D+$)/.test($("#MM").val())) {
+        $("#MM").css("border-color", "#F2272D");
+        $("#MMInfo").css("color", "#F2272D");
+        $("#MMInfo").html("密码至少包含数字和字母，请修改");
+        return false;
+    }
+    else {
+        $("#MM").css("border-color", "#999");
+        $("#MMInfo").html('');
+        return true;
+    }
+}
+
+function QRMMCheck() {
+    if (($("#QRMM").val().length < 6 && $("#QRMM").val().length > 0) || $("#QRMM").val().length > 20) {
+        $("#QRMM").css("border-color", "#F2272D");
+        $("#QRMMInfo").css("color", "#F2272D");
+        $("#QRMMInfo").html("确认密码为6-20个字符，请修改");
+        return false;
+    }
+    if ($("#QRMM").val().length === 0) {
+        $("#QRMM").css("border-color", "#F2272D");
+        $("#QRMMInfo").css("color", "#F2272D");
+        $("#QRMMInfo").html("请再次输入密码");
+        return false;
+    }
+    if ($("#QRMM").val() !== $("#MM").val()) {
+        $("#QRMM").css("border-color", "#F2272D");
+        $("#QRMMInfo").css("color", "#F2272D");
+        $("#QRMMInfo").html("两次输入的密码不一致，请重新输入");
+        return false;
+    }
+    else {
+        $("#QRMM").css("border-color", "#999");
+        $("#QRMMInfo").html('');
+        return true;
+    }
+}
+
+function GetCheckCode() {
+    if (SJCheck()) {
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: getRootPath() + "/Business/YHJBXX/GetYZM",
+            data: {
+                SJ: $("#SJ").val()
+            },
+            success: function (xml) {
+                if (xml.Result === 1) {
+                    GetNumber();
+                    return true;
+                }
+                else {
+                    alert("验证码发送失败");
+                    return false;
+                }
+            }
+        });
+    }
+}
+
+function GetNumber() {
+    $("#btnHQYZM").attr("disabled", "disabled");
+    $("#btnHQYZM").val(count + "S后重新获取");
+    count--;
+    if (count > 0) {
+        setTimeout(GetNumber, 1000);
+    }
+    else {
+        $("#btnHQYZM").val("获取验证码");
+        $("#btnHQYZM").removeAttr("disabled");
+        count = 60;
     }
 }
