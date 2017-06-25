543d67235bcde13148c18e4743136f67f2486825
rebirth033 <980381266@qq.com>
2017-06-25 23:16:56 +0800

    right

---
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Controllers/WDFBController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using TianWen.XiaoZhen1Qu.Web.Areas.Business.Common;
+
+namespace TianWen.XiaoZhen1Qu.Web.Areas.Business.Controllers
+{
+    public class WDFBController : BaseController
+    {
+        //public IYHDLXXBLL YHDLXXBLL { get; set; }
+
+        public ActionResult WDFB()
+        {
+            return View();
+        }
+    }
+}
