65f68874bba3045c218525e4a2026335931dab1e
rebirth033 <980381266@qq.com>
2017-06-26 22:13:40 +0800

    0626

---
+++ TianWen.XiaoZhen1Qu.Entities/Maps/CODES_Map.cs
@@ -0,0 +1,29 @@
+﻿using FluentNHibernate.Mapping;
+using TianWen.XiaoZhen1Qu.Entities.Models;
+
+namespace TianWen.XiaoZhen1Qu.Entities.Maps
+{
+    public class CODES_Map : ClassMap<CODES>
+    {
+        public CODES_Map()
+        {
+            Table("CODES");
+            #region 属性
+            Id(x => x.CODEID, "CODEID").GeneratedBy.Assigned().CustomType("AnsiString");
+            Map(x => x.TYPENAME, "TYPENAME");
+            Map(x => x.CODENAME, "CODENAME");
+            Map(x => x.CODEVALUE, "CODEVALUE");
+            Map(x => x.PARENTID, "PARENTID");
+            #endregion
+
+            #region OneToMany
+
+            #endregion
+
+            #region ManyToOne
+
+            #endregion
+
+        }
+    }
+}
---
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/LBXZ.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    LoadDL();
+});
+
+function LoadDL() {
+    $.ajax({
+        type: "POST",
+        url: getRootPath() + "/Business/LBXZ/LoadDL",
+        dataType: "json",
+        data:
+        {
+
+        },
+        success: function (xml) {
+            if (xml.Result === 1) {
+                alert("登录成功");
+            } else {
+                window.location.href = getRootPath() + "/Business/YHDLXX/YHDLXX";
+            }
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) { //有错误时的回调函数
+            _masker.CloseMasker(false, errorThrown);
+        }
+    });
+    $("#tableDL").html();
+}
