05693a399f9c247a994557a107f69f8a539a13a8
rebirth033 <980381266@qq.com>
2017-07-12 10:39:53 +0800

    0712

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/FWCZ.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/FWCZ.js
@@ -1,8 +1,5 @@
 ﻿$(document).ready(function () {
     $("#XQMC").bind("keyup", LoadXQMC);
-    $("#XQMC").bind("blur", ValidateXQMC);
-    $("#XQMC").bind("blur", HideXQMCList);
-    $("#XQMC").bind("focus", InfoXQMC);
     $("#spanCXLB").bind("click", CXLB);
     $("#imgZTCZ").bind("click", ZTCZSelect);
     $("#imgDJCZ").bind("click", DJCZSelect);
@@ -29,47 +26,6 @@
     LoadFWCZXX();
     FYMSSetDefault();
 });
-
-function ValidateXQMC()
-{
-    if ($("#XQMC").val() === "" || $("#XQMC").val() === null) {
-        $("#divXQMCTip").css("display", "block");
-        $("#divXQMCTip").attr("class", "Warn");
-        $("#divXQMCTip").html('<img src="http://10.0.1.165/XiaoZhen1Qu/Areas/Business/Css/images/warn.png" class="imgTip" />忘记填写小区名称啦');
-        $("#XQMC").css("border-color", "#fd634f");
-    } else {
-        $("#divXQMCTip").css("display", "none");
-        $("#XQMC").css("border-color", "#cccccc");
-    }
-}
-
-function InfoXQMC() {
-    $("#divXQMCTip").attr("class", "Info");
-    $("#divXQMCTip").html('<img src="http://10.0.1.165/XiaoZhen1Qu/Areas/Business/Css/images/info.png" class="imgTip" />2-20个汉字,不能填写电话、特殊符号');
-    $("#XQMC").css("border-color", "#5bc0de");
-}
-
-function HideXQMCList() {
-    $("#divXQMClist").css("display", "none");
-}
-
-function ValidateFWLX() {
-    if ($("#S").val() === "" || $("#S").val() === null) {
-        $("#divFWLXTip").css("display", "block");
-        $("#divFWLXTip").attr("class", "Warn");
-        $("#divXQMCTip").html('<img src="http://10.0.1.165/XiaoZhen1Qu/Areas/Business/Css/images/warn.png" class="imgTip" />忘记填写小区名称啦');
-        $("#XQMC").css("border-color", "#fd634f");
-    } else {
-        $("#divXQMCTip").css("display", "none");
-        $("#XQMC").css("border-color", "#cccccc");
-    }
-}
-
-function InfoFWLX() {
-    $("#divXQMCTip").attr("class", "Info");
-    $("#divXQMCTip").html('<img src="http://10.0.1.165/XiaoZhen1Qu/Areas/Business/Css/images/info.png" class="imgTip" />2-20个汉字,不能填写电话、特殊符号');
-    $("#XQMC").css("border-color", "#5bc0de");
-}
 
 //上传照片
 function Upload() {
---
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/FWCZYZ.js
@@ -0,0 +1,47 @@
+﻿$(document).ready(function () {
+    $("#XQMC").bind("blur", ValidateXQMC);
+    $("#XQMC").bind("blur", HideXQMCList);
+    $("#XQMC").bind("focus", InfoXQMC);
+    $("#S").bind("blur", ValidateFWLX);
+});
+
+function ValidateXQMC() {
+    if ($("#XQMC").val() === "" || $("#XQMC").val() === null) {
+        $("#divXQMCTip").css("display", "block");
+        $("#divXQMCTip").attr("class", "Warn");
+        $("#divXQMCTip").html('<img src="http://10.0.1.165/XiaoZhen1Qu/Areas/Business/Css/images/warn.png" class="imgTip" />忘记填写小区名称啦');
+        $("#XQMC").css("border-color", "#fd634f");
+    } else {
+        $("#divXQMCTip").css("display", "none");
+        $("#XQMC").css("border-color", "#cccccc");
+    }
+}
+
+function InfoXQMC() {
+    $("#divXQMCTip").attr("class", "Info");
+    $("#divXQMCTip").html('<img src="http://10.0.1.165/XiaoZhen1Qu/Areas/Business/Css/images/info.png" class="imgTip" />2-20个汉字,不能填写电话、特殊符号');
+    $("#XQMC").css("border-color", "#5bc0de");
+}
+
+function HideXQMCList() {
+    $("#divXQMClist").css("display", "none");
+}
+
+
+function ValidateFWLX() {
+    if ($("#S").val() === "" || $("#S").val() === null) {
+        $("#divFWLXTip").css("display", "block");
+        $("#divFWLXTip").attr("class", "Warn");
+        $("#divXQMCTip").html('<img src="http://10.0.1.165/XiaoZhen1Qu/Areas/Business/Css/images/warn.png" class="imgTip" />忘记填写小区名称啦');
+        $("#XQMC").css("border-color", "#fd634f");
+    } else {
+        $("#divXQMCTip").css("display", "none");
+        $("#XQMC").css("border-color", "#cccccc");
+    }
+}
+
+function InfoFWLX() {
+    $("#divXQMCTip").attr("class", "Info");
+    $("#divXQMCTip").html('<img src="http://10.0.1.165/XiaoZhen1Qu/Areas/Business/Css/images/info.png" class="imgTip" />2-20个汉字,不能填写电话、特殊符号');
+    $("#XQMC").css("border-color", "#5bc0de");
+}
