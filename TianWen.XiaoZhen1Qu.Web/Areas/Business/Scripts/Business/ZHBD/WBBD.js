b69e40f67753b20e327842efe40145123ffe34a9
rebirth033 <980381266@qq.com>
2017-07-29 15:42:23 +0800

    0729

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/ZHBD/WBBD.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/ZHBD/WBBD.js
@@ -2,59 +2,70 @@
     $("#span_head_right_zc").bind("click", ToWBZC);
     $("#input_bottom_qx").bind("click", Back);
     $("#input_bottom_dl").bind("click", WBDL);
-    $("[data-toggle='tooltip']").tooltip();
-    var ZHoptions = {
-        title: "<li style='text-align:left;'>请再次输入你的账号</li>",
-        animation: true,
-        html: true,
-        placement: 'right',
-        trigger: 'focus'
-    }
-    $("#inputZH").tooltip(ZHoptions);
-});
+    $('#inputZH').bind("focus", { id: "inputZH" }, HideTip);
+    $('#inputMM').bind("focus", { id: "inputMM" }, HideTip);
+    BindToolTip();
 
+});
+//进入微博注册页面
 function ToWBZC() {
     window.open("https://login.sina.com.cn/signup/signup?entry=homepage");
 }
-
+//返回
 function Back() {
     window.location.href = getRootPath() + "/Business/ZHBD/ZHBD?YHID=" + getUrlParam("YHID");
 }
-
+//微博登录
 function WBDL() {
     if (Validate() === false) return;
-}
 
+}
+//账号检查
 function ZHCheck() {
     if ($("#inputZH").val().length === 0) {
-        $("#inputZH").css("border-color", "#F2272D");
-        $("#ZHInfo").css("color", "#F2272D");
-        $("#ZHInfo").html("请输入账号");
+        $('#inputZH').tooltip('show');
         return false;
     }
     else {
-        $("#inputZH").css("border-color", "#999");
-        $("#ZHInfo").html('<img src=' + getRootPath() + '/Areas/Business/Css/images/yes.png />');
         return true;
     }
 }
-
+//密码检查
 function MMCheck() {
     if ($("#inputMM").val().length === 0) {
-        $("#inputMM").css("border-color", "#F2272D");
-        $("#MMInfo").css("color", "#F2272D");
-        $("#MMInfo").html("请输入账号");
+        $('#inputMM').tooltip('show');
         return false;
     }
     else {
-        $("#inputMM").css("border-color", "#999");
-        $("#MMInfo").html('<img src=' + getRootPath() + '/Areas/Business/Css/images/yes.png />');
         return true;
     }
 }
-
+//登录验证
 function Validate() {
     if (!ZHCheck()) return false;
     if (!MMCheck()) return false;
     return true;
+}
+//绑定tooltip
+function BindToolTip() {
+    var ZHoptions = {
+        title: "<li class='li_div_body'>请输入你的微博账号</li>",
+        animation: true,
+        html: true,
+        placement: 'right',
+        trigger: 'manual'
+    }
+    $("#inputZH").tooltip(ZHoptions);
+    var MMoptions = {
+        title: "<li class='li_div_body'>请输入你的密码</li>",
+        animation: true,
+        html: true,
+        placement: 'right',
+        trigger: 'manual'
+    }
+    $("#inputMM").tooltip(MMoptions);
+}
+//隐藏tooltip
+function HideTip(obj) {
+    $("#" + obj.data.id).tooltip('hide');
 }
