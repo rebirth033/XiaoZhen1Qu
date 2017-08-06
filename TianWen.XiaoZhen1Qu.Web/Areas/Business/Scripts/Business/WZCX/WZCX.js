294a4e0f1eead7fca58706ac3decb02eefb5b79a
rebirth033 <980381266@qq.com>
2017-08-06 22:48:29 +0800

    违章查询页面完成

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/WZCX/WZCX.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/WZCX/WZCX.js
@@ -3,8 +3,12 @@
     $("#divCPHText").bind("click", LoadSJXZQJC);
     $("#divCPHSZMText").bind("click", LoadCPHSZM);
     $("body").bind("click", CloseXZQ);
+    $("#input_content_mfcx").bind("click", MFCX);
+    $("#inputCPHHWW").bind("blur", CPHCheck);
+    $("#inputCJH").bind("blur", CJHCheck);
+    $("#inputFDJH").bind("blur", FDJHCheck);
 });
-
+//根据级别获取行政区
 function LoadXZQByGrade() {
     $.ajax({
         type: "POST",
@@ -32,7 +36,7 @@ function LoadXZQByGrade() {
         }
     });
 }
-
+//获取市
 function GetCitys(CODE) {
     $.ajax({
         type: "POST",
@@ -58,27 +62,27 @@ function GetCitys(CODE) {
         }
     });
 }
-
+//选择行政区
 function SelectXZQ(NAME) {
     $("#span_content_info_xzq").html(NAME);
     $("#div_XZQ").css("display", "none");
 }
-
+//获取行政区简称
 function SelectXZQJC(NAME) {
     $("#spanXZQJC").html(NAME);
     $("#div_XZQJC").css("display", "none");
 }
-
+//选择车牌号首字母
 function SelectCPHSZM(SZM) {
     $("#spanCPHSZM").html(SZM);
     $("#div_CPHSZM").css("display", "none");
 }
-
+//关闭行政区
 function CloseXZQ() {
     $("#div_XZQ").css("display", "none");
     $("#div_XZQJC").css("display", "none");
 }
-
+//加载省级行政区简称
 function LoadSJXZQJC() {
     $.ajax({
         type: "POST",
@@ -103,7 +107,7 @@ function LoadSJXZQJC() {
         }
     });
 }
-
+//加载车牌号首字母
 function LoadCPHSZM() {
     var SZMs = new Array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
     var html = "";
@@ -112,4 +116,76 @@ function LoadCPHSZM() {
     }
     $("#div_CPHSZM").html(html);
     $("#div_CPHSZM").css("display", "block");
+}
+//车牌号检查
+function CPHCheck() {
+    var CPH = $("#spanXZQJC").html() + $("#spanCPHSZM").html() + $("#inputCPHHWW").val();
+    if (!ValidateCPH(CPH) && $("#inputCPHHWW").val().length !== 0) {
+        $("#inputCPHHWW").css("border-color", "#F2272D");
+        $("#span_CPHInfo").css("color", "#F2272D");
+        $("#span_CPHInfo").html("车牌号输入错误");
+        return false;
+    }
+    else if ($("#inputCPHHWW").val().length === 0) {
+        $("#inputCPHHWW").css("border-color", "#F2272D");
+        $("#span_contespan_CPHInfont_info_llsj").css("color", "#F2272D");
+        $("#span_CPHInfo").html("请输入车牌号");
+        return false;
+    }
+    else {
+        $("#inputCPHHWW").css("border-color", "#ccc");
+        $("#span_CPHInfo").html("");
+        return true;
+    }
+}
+//车架号检查
+function CJHCheck() {
+    if (!(/^\d{6}\b/.test($("#inputCJH").val())) && $("#inputCJH").val().length !== 0) {
+        $("#inputCJH").css("border-color", "#F2272D");
+        $("#span_CJHInfo").css("color", "#F2272D");
+        $("#span_CJHInfo").html("车架号输入错误");
+        return false;
+    }
+    else if ($("#inputCJH").val().length === 0) {
+        $("#inputCJH").css("border-color", "#F2272D");
+        $("#span_CJHInfo").css("color", "#F2272D");
+        $("#span_CJHInfo").html("请输入车架号");
+        return false;
+    }
+    else {
+        $("#inputCJH").css("border-color", "#ccc");
+        $("#span_CJHInfo").html("");
+        return true;
+    }
+}
+//发动机号检查
+function FDJHCheck() {
+    if (!(/^\d{6}\b/.test($("#inputFDJH").val())) && $("#inputFDJH").val().length !== 0) {
+        $("#inputFDJH").css("border-color", "#F2272D");
+        $("#span_FDJHInfo").css("color", "#F2272D");
+        $("#span_FDJHInfo").html("发动机号输入错误");
+        return false;
+    }
+    else if ($("#inputFDJH").val().length === 0) {
+        $("#inputFDJH").css("border-color", "#F2272D");
+        $("#span_FDJHInfo").css("color", "#F2272D");
+        $("#span_FDJHInfo").html("请输入发动机号");
+        return false;
+    }
+    else {
+        $("#inputFDJH").css("border-color", "#ccc");
+        $("#span_FDJHInfo").html("");
+        return true;
+    }
+}
+//免费查询验证
+function MFCXValidate() {
+    if (!CPHCheck()) return false;
+    if (!CJHCheck()) return false;
+    if (!FDJHCheck()) return false;
+    return true;
+}
+//免费查询
+function MFCX() {
+    if (!MFCXValidate()) return;
 }
