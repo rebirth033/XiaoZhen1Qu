e00c935358e731077c86310497e162f4fdd7930e
rebirth033 <980381266@qq.com>
2017-08-07 17:38:09 +0800

    0807

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/BZZX/BZZX.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/BZZX/BZZX.js
@@ -3,6 +3,7 @@
     $("#span_head_nav_info_cjwt").bind("click", ToCJWT);
     $("#span_head_nav_info_lxkf").bind("click", ToLXKF);
     $("#span_head_nav_info_wzjy").bind("click", ToWZJY);
+    ToSY();
 });
 
 //帮助中心_首页
@@ -11,16 +12,16 @@ function ToSY() {
 }
 
 //帮助中心_常见问题
-function ToSY() {
+function ToCJWT() {
     $("#iframeright").attr("src", getRootPath() + "/Business/BZZX/BZZX_CJWT?YHID=" + getUrlParam("YHID"));
 }
 
 //帮助中心_联系客服
-function ToSY() {
+function ToLXKF() {
     $("#iframeright").attr("src", getRootPath() + "/Business/BZZX/BZZX_LXKF?YHID=" + getUrlParam("YHID"));
 }
 
 //帮助中心_网站建议
-function ToSY() {
+function ToWZJY() {
     $("#iframeright").attr("src", getRootPath() + "/Business/BZZX/BZZX_WZJY?YHID=" + getUrlParam("YHID"));
 }
