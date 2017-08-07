c54313886df9cdeb67cd582c941f59b09a9194e6
rebirth033 <980381266@qq.com>
2017-08-07 18:21:09 +0800

    0807

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/BZZX/BZZX_SY.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/BZZX/BZZX_SY.js
@@ -1,1 +1,16 @@
-﻿
+﻿$(document).ready(function () {
+    $(".div_left_box_info").bind("mouseover", ShowRemark);
+    $(".div_left_box_info").bind("mouseleave", HideRemark);
+});
+
+function ShowRemark() {
+    $(this).find(".span_left_box_info_remark").each(function() {
+        $(this).css("display", "block");
+    });
+}
+
+function HideRemark() {
+    $(this).find(".span_left_box_info_remark").each(function () {
+        $(this).css("display", "none");
+    });
+}
