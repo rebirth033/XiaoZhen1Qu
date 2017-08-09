ec48a818a7f079f991448a117c98b8bc1366d011
rebirth033 <980381266@qq.com>
2017-08-09 11:15:20 +0800

    用户管理页面调整

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/YHGL.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/YHGL.js
@@ -21,6 +21,8 @@
     $("#spanZWGL").parent().bind("click", { type: "ZWGL" }, ExpandThird);
     $("#spanJLGL").parent().bind("click", { type: "JLGL" }, ExpandThird);
     $("#spanZHXX").parent().bind("click", { type: "ZHXX" }, ExpandThird);
+    $("#spanXJ").parent().bind("click", { type: "XJ" }, ExpandSecond_Leaf);
+    $("#spanTGB").parent().bind("click", { type: "TGB" }, ExpandSecond_Leaf);
     $(".li_left_menu").bind("click", LiFocus);
     $("#span_close").bind("click", CloseQQRZ);
     $("#div_body_image_qqtx").bind("mouseover", ShowQQ);
