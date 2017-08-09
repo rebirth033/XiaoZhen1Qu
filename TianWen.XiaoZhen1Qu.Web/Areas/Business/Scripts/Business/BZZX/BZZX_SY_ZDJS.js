e08576b4a7cff85a81c4cef668bc8f74c4236ce1
rebirth033 <980381266@qq.com>
2017-08-09 09:45:00 +0800

    菜单定位文本

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/BZZX/BZZX_SY_ZDJS.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/BZZX/BZZX_SY_ZDJS.js
@@ -1,1 +1,8 @@
-﻿
+﻿$(document).ready(function () {
+    $(".span_body_nav").bind("click", GoToTip);
+});
+
+function GoToTip() {
+    var len = document.getElementById("div_body_nav_title_" + this.id).offsetTop;//获取div层到页面顶部的高度 
+    $("html,body").stop().animate({ scrollTop: len }, 300, "swing", function () {});
+}
