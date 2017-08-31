924dd7511a21098d327f1d4089e7a02713c1e832
rebirth033 <980381266@qq.com>
2017-08-31 21:22:50 +0800

    0831

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/BZZX/BZZX_LXKF.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/BZZX/BZZX_LXKF.js
@@ -1,15 +1,29 @@
 ﻿$(document).ready(function () {
     $(".span_wtlx_inner_right").bind("click", SelectWTLX);
     $("#span_wtlx_inner_right_xxbsc").bind("click", showXXBSC);
+    bindHover();
 });
 
 function SelectWTLX() {
     $(".span_wtlx_inner_right").each(function () {
         $(this).css("background-color", "#eBeBeB").css("font-weight", "normal").css("color", "#000");
     });
+    bindHover();
     $(this).css("background-color", "#ff6a00").css("font-weight", "700").css("color", "#fff");
+    $(this).unbind("mouseleave");
 }
 
 function showXXBSC() {
     $("#span_step_text_second").html("请输入您的信息编号:")
+}
+
+function bindHover() {
+    $(".span_wtlx_inner_right").each(function () {
+        $(this).bind("mouseover", function () {
+            $(this).css("background-color", "#ff6a00").css("font-weight", "700").css("color", "#fff");
+        })
+        $(this).bind("mouseleave", function () {
+            $(this).css("background-color", "#eBeBeB").css("font-weight", "normal").css("color", "#000");
+        })
+    });
 }
