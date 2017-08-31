e391e95c6a296caaf74e65c529a66a961e0bf646
rebirth033 <980381266@qq.com>
2017-08-31 15:53:09 +0800

    0831

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/BZZX/BZZX_LXKF.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/BZZX/BZZX_LXKF.js
@@ -1,1 +1,10 @@
-﻿
+﻿$(document).ready(function () {
+    $(".span_wtlx_inner_right").bind("click", SelectWTLX);
+});
+
+function SelectWTLX() {
+    $(".span_wtlx_inner_right").each(function () {
+        $(this).css("background-color", "#eBeBeB").css("font-weight", "normal").css("color", "#000");
+    });
+    $(this).css("background-color", "#ff6a00").css("font-weight", "700").css("color", "#fff");
+}
