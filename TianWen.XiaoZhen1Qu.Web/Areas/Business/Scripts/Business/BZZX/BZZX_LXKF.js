bcc28f7c8eacaff93931f1e7109a30f9759a4f40
rebirth033 <980381266@qq.com>
2017-08-31 16:59:40 +0800

    0831

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/BZZX/BZZX_LXKF.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/BZZX/BZZX_LXKF.js
@@ -1,5 +1,6 @@
 ﻿$(document).ready(function () {
     $(".span_wtlx_inner_right").bind("click", SelectWTLX);
+    $("#span_wtlx_inner_right_xxbsc").bind("click", showXXBSC);
 });
 
 function SelectWTLX() {
@@ -7,4 +8,8 @@ function SelectWTLX() {
         $(this).css("background-color", "#eBeBeB").css("font-weight", "normal").css("color", "#000");
     });
     $(this).css("background-color", "#ff6a00").css("font-weight", "700").css("color", "#fff");
+}
+
+function showXXBSC() {
+    $("#span_step_text_second").html("请输入您的信息编号:")
 }
