24b159991524fb74fb9e78a9744e4b652a73cfa4
rebirth033 <980381266@qq.com>
2017-09-01 10:44:39 +0800

    0901

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/BZZX/BZZX_LXKF.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/BZZX/BZZX_LXKF.js
@@ -1,6 +1,7 @@
 ﻿$(document).ready(function () {
     $(".span_wtlx_inner_right").bind("click", SelectWTLX);
     bindHover();
+    BindWTMS();
 });
 //选择问题类型
 function SelectWTLX() {
@@ -54,15 +55,65 @@ function showWTLX(id) {
         ShowXXBSC();
     if (id.indexOf("xxbfhxg") != -1)
         ShowXXBFHXG();
+    if (id.indexOf("xxbpflbd") != -1)
+        ShowXXBPFLDB();
+    if (id.indexOf("xxbxs") != -1)
+        ShowXXBXS();
+    if (id.indexOf("tsjbxjxx") != -1)
+        ShowTSJBXJXX();
+    if (id.indexOf("mysjhm") != -1)
+        ShowMYSJHM();
 }
 //信息被删除
 function ShowXXBSC() {
-    $("#span_step_text_second").html("请输入您的信息编号：")
+    $("#span_step_text_second").html("请输入您的信息编号：");
     $("#div_xxbsc").css("display", "block");
     $("#btnCKBSCYY").bind("click", CKBSCYY);
 }
 //信息被返回修改
 function ShowXXBFHXG() {
-    $("#span_step_text_second").html("请描述您遇到的问题：")
+    $("#span_step_text_second").html("请描述您遇到的问题：");
     $("#div_xxbfhxg").css("display", "block");
+    BindWTMS();
+}
+//信息被判分类不当
+function ShowXXBPFLDB() {
+    $("#span_step_text_second").html("请描述您遇到的问题：");
+    $("#div_xxbfhxg").css("display", "block");
+    BindWTMS();
+}
+//信息不显示
+function ShowXXBXS() {
+    $("#span_step_text_second").html("请描述您遇到的问题：");
+    $("#div_xxbfhxg").css("display", "block");
+    BindWTMS();
+}
+//投诉举报虚假信息
+function ShowTSJBXJXX() {
+    $("#span_step_text_second").html("请描述您遇到的问题：");
+    $("#div_xxbfhxg").css("display", "block");
+    BindWTMS();
+}
+//冒用手机号码
+function ShowMYSJHM() {
+    $("#span_step_text_second").html("请描述您遇到的问题：");
+    $("#div_xxbfhxg").css("display", "block");
+    BindWTMS();
+}
+
+function BindWTMS() {
+    $("#textarea_mswt").bind("focus", WTMSFocus);
+    $("#textarea_mswt").bind("blur", WTMSBlur);
+}
+
+function WTMSFocus() {
+    $("#textarea_mswt").css("color", "#333333");
+    $("#textarea_mswt").html("");
+}
+
+function WTMSBlur() {
+    $("#textarea_mswt").css("color", "#999999");
+    if ($("#textarea_mswt").html() === "") {
+        $("#textarea_mswt").html("请描述您的具体问题，字数在15至300之间");
+    }
 }
