86c5625883818bde517749e9f03bcbfa7f5359e7
rebirth033 <980381266@qq.com>
2017-09-01 09:42:52 +0800

    0901

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/BZZX/BZZX_LXKF.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/BZZX/BZZX_LXKF.js
@@ -12,6 +12,7 @@ function SelectWTLX() {
     bindHover();
     $(this).css("background-color", "#ff6a00").css("font-weight", "700").css("color", "#fff");
     $(this).unbind("mouseleave");
+    showWTLX(this.id);
 }
 
 function showXXBSC() {
@@ -34,7 +35,7 @@ function CKBSCYY() {
 
 }
 
-//用户名检查
+//信息编号检查
 function XXBHCheck() {
     if ($("#inputXXBH").val().length === 0) {
         $("#inputXXBH").css("border-color", "#F2272D");
@@ -43,7 +44,23 @@ function XXBHCheck() {
     }
     else {
         $("#inputXXBH").css("border-color", "#999");
-        $("#td_xxbhinfo").html("<div class=\"div_bscyy\"></div>");
+        $("#td_xxbhinfo").html("<textarea class=\"textarea_bscyy\"></textarea><span>以上信息没有解决您的问题？</span><input class=\"btn btn-info\" type=\"button\" style=\"margin-top: -3px; width: 140px;\" value=\"填表单，联系客服\" onclick=\"TBD()\" />");
         return true;
     }
+}
+
+function TBD() {
+    $("#div_content_tjkf").css("display", "block");
+}
+
+function showWTLX(id) {
+    $(".div_wtjj_inner").each(function () {
+        $(this).css("display", "none");
+    });
+    if (id.indexOf("xxbsc") != -1)
+        ShowXXBSC();
+}
+
+function ShowXXBSC() {
+    $("#div_xxbsc").css("display", "block");
 }
