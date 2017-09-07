3e5101d8cb63b7a343d4662451d72ec0e4b6f095
rebirth033 <980381266@qq.com>
2017-09-07 11:54:32 +0800

    0907

---
+++ 网络爬虫_获取小区信息/WLPC/Ashx/GetXQXX.ashx.cs
@@ -0,0 +1,85 @@
+﻿using System.Net;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Web;
+using System.Data.OracleClient;
+
+namespace WLPC.Ashx
+{
+    /// <summary>
+    /// GetXQXX 的摘要说明
+    /// </summary>
+    public class GetXQXX : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string url = context.Request["lianjie"];
+            string page = context.Request["page"];
+            string area = context.Request["area"];
+            string pageinfo, childpageinfo;
+            try
+            {
+                pageinfo = GetPageInfo(url);
+
+                string connString = "User ID=c##XIAOZHEN1QU;Password=XIAOZHEN1QU;Data Source=ORCL;";
+                OracleConnection conn = new OracleConnection(connString);
+                OracleCommand command = new OracleCommand();
+                command.Connection = conn;
+                conn.Open();
+
+                string pattern = "<em><a(\\s+(href=\"(?<url>([^\"])*)\"|'([^'])*'|\\w+=\"(([^\"])*)\"|'([^'])*'))+>(?<text>(.*?))</a></em>";
+                Regex regex = new Regex(pattern);
+                MatchCollection matches = regex.Matches(pageinfo);
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    string address = string.Empty;
+                    childpageinfo = GetPageInfo(matches[i].Groups["url"].Value);
+                    pattern = "<p(\\s+(class=\"address\"|'([^'])*'|\\w+=\"(([^\"])*)\"|'([^'])*'))+>(?<text>(.*?))</p>";
+                    regex = new Regex(pattern);
+                    MatchCollection childmatches = regex.Matches(childpageinfo);
+                    for (int j = 0; j < childmatches.Count; j++)
+                    {
+                        if (childmatches[j].ToString().Contains("address"))
+                        {
+                            address = childmatches[j].Groups["text"].Value;
+                            break;
+                        }
+                    }
+                    if (!string.IsNullOrEmpty(address))
+                        address = address.Substring(address.IndexOf('[')+1, address.Length - address.IndexOf('[') - 1-1);
+                    string sql = "insert into xqjbxx values(s_xqjbxx.nextval,'" + matches[i].Groups["text"].Value + "','"+ address + "','','','','" + area + "','" + page + "')";
+                    command.CommandText = sql;
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                context.Response.Write(ex.Message);
+            }
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("完成");
+        }
+
+        public string GetPageInfo(string url)
+        {
+            HttpWebRequest myReq = (HttpWebRequest)HttpWebRequest.Create(url);
+            myReq.Accept = "image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, application/x-shockwave-flash, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, */*";
+            myReq.UserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727)";
+            HttpWebResponse myRep = (HttpWebResponse)myReq.GetResponse();
+            Stream myStream = myRep.GetResponseStream();
+            StreamReader sr = new StreamReader(myStream, Encoding.UTF8);
+            return sr.ReadToEnd();
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
