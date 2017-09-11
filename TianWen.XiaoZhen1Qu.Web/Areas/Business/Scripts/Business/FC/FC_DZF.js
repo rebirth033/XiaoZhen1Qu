4318f12b06371d1af193cfcf8b47ccc11f0ef28e
rebirth033 <980381266@qq.com>
2017-09-11 10:11:25 +0800

    0911

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/FC/FC_DZF.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/FC/FC_DZF.js
@@ -1,10 +1,10 @@
 ﻿var isleave = true;
 var ue = UE.getEditor('FYMS');
 $(document).ready(function () {
-    $(".div_top_left").css("margin-left", (document.documentElement.clientWidth - 1084) / 2);
-    $(".div_top_right").css("margin-right", (document.documentElement.clientWidth - 1084) / 2);
-    $(".div_head").css("margin-left", (document.documentElement.clientWidth - 1084) / 2);
-    $(".div_content").css("margin-left", (document.documentElement.clientWidth - 1084) / 2);
+    $(".div_top_left").css("margin-left", (document.documentElement.clientWidth - 900) / 2);
+    $(".div_top_right").css("margin-right", (document.documentElement.clientWidth - 900) / 2);
+    $(".div_head").css("margin-left", (document.documentElement.clientWidth - 900) / 2);
+    $(".div_content").css("margin-left", (document.documentElement.clientWidth - 900) / 2);
     $("#spanCXLB").bind("click", CXLB);
     $("#imgDZF").bind("click", DZFSelect);
     $("#imgRZF").bind("click", RZFSelect);
--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/FC/FC_FW.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/FC/FC_FW.js
@@ -1,9 +1,9 @@
 ﻿var isleave = true;
 $(document).ready(function () {
-    $(".div_top_left").css("margin-left", (document.documentElement.clientWidth - 1084) / 2);
-    $(".div_top_right").css("margin-right", (document.documentElement.clientWidth - 1084) / 2);
-    $(".div_head").css("margin-left", (document.documentElement.clientWidth - 1084) / 2);
-    $(".div_content").css("margin-left", (document.documentElement.clientWidth - 1084) / 2);
+    $(".div_top_left").css("margin-left", (document.documentElement.clientWidth - 900) / 2);
+    $(".div_top_right").css("margin-right", (document.documentElement.clientWidth - 900) / 2);
+    $(".div_head").css("margin-left", (document.documentElement.clientWidth - 900) / 2);
+    $(".div_content").css("margin-left", (document.documentElement.clientWidth - 900) / 2);
     $("#XQMC").bind("keyup", LoadXQMC);
     $("#spanCXLB").bind("click", CXLB);
     $("#imgZTCZ").bind("click", ZTCZSelect);
