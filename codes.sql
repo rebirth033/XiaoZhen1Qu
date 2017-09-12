8e7b2e1f47d28a83fbcc8d2123b867415c1e98b5
rebirth033 <980381266@qq.com>
2017-09-12 22:58:13 +0800

    0912

---
+++ codes.sql
@@ -0,0 +1,40 @@
+prompt Importing table codes...
+set feedback off
+set define off
+insert into codes (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (102, '新旧程度', '全新', '1', 1, null);
+
+insert into codes (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (103, '新旧程度', '95成新', '2', 2, null);
+
+insert into codes (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (104, '新旧程度', '9成新', '3', 3, null);
+
+insert into codes (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (105, '新旧程度', '8成新', '4', 4, null);
+
+insert into codes (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (106, '新旧程度', '7成新及以下', '5', 5, null);
+
+insert into codes (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (107, '笔记本配件', '笔记本包', '1', 1, null);
+
+insert into codes (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (108, '笔记本配件', '笔记本电源', '2', 2, null);
+
+insert into codes (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (109, '笔记本配件', '笔记本电池', '3', 3, null);
+
+insert into codes (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (110, '笔记本配件', '支架/散热垫', '4', 4, null);
+
+insert into codes (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (111, '笔记本配件', '笔记本内存', '5', 5, null);
+
+insert into codes (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (112, '笔记本配件', '笔记本硬盘', '6', 6, null);
+
+insert into codes (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (113, '笔记本配件', '其他配件', '7', 7, null);
+
+prompt Done.
