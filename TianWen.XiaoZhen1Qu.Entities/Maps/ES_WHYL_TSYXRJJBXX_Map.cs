9b9898332dd6114cd49939b2f0347f843544b352
rebirth033 <980381266@qq.com>
2017-09-15 17:02:13 +0800

    0915

---
+++ TianWen.XiaoZhen1Qu.Entities/Maps/ES_WHYL_TSYXRJJBXX_Map.cs
@@ -0,0 +1,34 @@
+﻿using FluentNHibernate.Mapping;
+using TianWen.XiaoZhen1Qu.Entities.Models;
+
+namespace TianWen.XiaoZhen1Qu.Entities.Maps
+{
+    public class ES_WHYL_TSYXRJJBXX_Map : ClassMap<ES_WHYL_TSYXRJJBXX>
+    {
+        public ES_WHYL_TSYXRJJBXX_Map()
+        {
+            Table("ES_WHYL_TSYXRJJBXX");
+            #region 属性
+            Id(x => x.ES_WHYL_TSYXRJJBXXID, "ES_WHYL_TSYXRJJBXXID").GeneratedBy.Assigned().CustomType("AnsiString");
+            Map(x => x.JCXXID, "JCXXID");
+            Map(x => x.GQ, "GQ");
+            Map(x => x.LB, "LB");
+            Map(x => x.XL, "XL");
+            Map(x => x.XJ, "XJ");
+            Map(x => x.JG, "JG");
+            Map(x => x.BCMS, "BCMS");
+            Map(x => x.JYQY, "JYQY");
+            Map(x => x.JYDD, "JYDD");
+            #endregion
+
+            #region OneToMany
+
+            #endregion
+
+            #region ManyToOne
+
+            #endregion
+
+        }
+    }
+}
---
+++ TianWen.XiaoZhen1Qu/BLL/ES_WHYL_TSYXRJBLL.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using NHibernate;
+using TianWen.Framework.Log;
+using TianWen.XiaoZhen1Qu.Entities.Models;
+using TianWen.XiaoZhen1Qu.Interface;
+
+namespace TianWen.XiaoZhen1Qu.BLL
+{
+    public class ES_WHYL_TSYXRJBLL : BaseBLL, IES_WHYL_TSYXRJBLL
+    {
+        public object SaveES_WHYL_TSYXRJJBXX(JCXX jcxx, ES_WHYL_TSYXRJJBXX ES_WHYL_TSYXRJJBXX, List<PHOTOS> photos)
+        {
+            string[] photoNames = photos.Select(x => x.PHOTONAME).ToArray();
+            DirectoryInfo TheFolder = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory + "/Areas/Business/Photos/");
+            FileInfo[] fileinfos = TheFolder.GetFiles();
+            DataTable dt = DAO.Repository.GetDataTable(string.Format("SELECT * FROM ES_WHYL_TSYXRJJBXX WHERE ES_WHYL_TSYXRJJBXXID='{0}'", ES_WHYL_TSYXRJJBXX.ES_WHYL_TSYXRJJBXXID));
+
+            if (dt.Rows.Count > 0)
+            {
+                using (ITransaction transaction = DAO.BeginTransaction())
+                {
+                    try
+                    {
+                        if (photos.Count > 0)
+                        {
+                            //照片全删全插
+                            List<PHOTOS> old_photos = DAO.GetObjectList<PHOTOS>(string.Format("FROM PHOTOS WHERE JCXXID='{0}'", dt.Rows[0]["JCXXID"])).ToList();
+                            foreach (var obj in old_photos)
+                            {
+                                DAO.Remove(obj);
+                            }
+                        }
+                        foreach (var obj in photos)
+                        {
+                            obj.JCXXID = dt.Rows[0]["JCXXID"].ToString();
+                            DAO.Save(obj);
+                        }
+
+                        if (photos.Count > 0)
+                        {
+                            //删除多余照片文件
+                            foreach (FileInfo fileobj in fileinfos)
+                            {
+                                if (!photoNames.Contains(fileobj.Name))
+                                    File.Delete(AppDomain.CurrentDomain.BaseDirectory + "/Areas/Business/Photos/" + fileobj.Name);
+                            }
+                        }
+
+                        ES_WHYL_TSYXRJJBXX.JCXXID = dt.Rows[0]["JCXXID"].ToString();
+                        jcxx.JCXXID = dt.Rows[0]["JCXXID"].ToString();
+                        DAO.Update(jcxx);
+                        DAO.Update(ES_WHYL_TSYXRJJBXX);
+                        transaction.Commit();
+                        return new { Result = EnResultType.Success, Message = "修改成功!", Value = new { JCXXID = jcxx.JCXXID, ES_WHYL_TSYXRJJBXXID = ES_WHYL_TSYXRJJBXX.ES_WHYL_TSYXRJJBXXID } };
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        LoggerManager.Error("ES_WHYL_TSYXRJJBXXBLL", "保存失败【" + ex.Message + "\r\n" + ex.StackTrace + "】!");
+                        return new
+                        {
+                            Result = EnResultType.Failed,
+                            Message = "保存失败【" + ex.Message + "\r\n" + ex.StackTrace + "】!",
+                            Type = 3
+                        };
+                    }
+                }
+            }
+            else
+            {
+                using (ITransaction transaction = DAO.BeginTransaction())
+                {
+                    try
+                    {
+                        ES_WHYL_TSYXRJJBXX.JCXXID = jcxx.JCXXID;
+                        DAO.Save(jcxx);
+                        DAO.Save(ES_WHYL_TSYXRJJBXX);
+                        //照片全删全插
+                        List<PHOTOS> old_photos = DAO.GetObjectList<PHOTOS>(string.Format("FROM PHOTOS WHERE JCXXID='{0}'", ES_WHYL_TSYXRJJBXX.JCXXID)).ToList();
+                        foreach (var obj in old_photos)
+                        {
+                            DAO.Remove(obj);
+                        }
+                        foreach (var obj in photos)
+                        {
+                            obj.JCXXID = ES_WHYL_TSYXRJJBXX.JCXXID;
+                            DAO.Save(obj);
+                        }
+                        transaction.Commit();
+                        return new { Result = EnResultType.Success, Message = "新增成功!", Value = new { JCXXID = jcxx.JCXXID, ES_WHYL_TSYXRJJBXXID = ES_WHYL_TSYXRJJBXX.ES_WHYL_TSYXRJJBXXID } };
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        LoggerManager.Error("ES_WHYL_TSYXRJJBXXBLL", "保存失败【" + ex.Message + "\r\n" + ex.StackTrace + "】!");
+                        return new
+                        {
+                            Result = EnResultType.Failed,
+                            Message = "保存失败【" + ex.Message + "\r\n" + ex.StackTrace + "】!",
+                            Type = 3
+                        };
+                    }
+                }
+            }
+        }
+
+        public object LoadES_WHYL_TSYXRJJBXX(string ES_WHYL_TSYXRJJBXXID)
+        {
+            try
+            {
+                ES_WHYL_TSYXRJJBXX ES_WHYL_TSYXRJJBXX = DAO.GetObjectByID<ES_WHYL_TSYXRJJBXX>(ES_WHYL_TSYXRJJBXXID);
+                if (ES_WHYL_TSYXRJJBXX != null)
+                {
+                    JCXX jcxx = GetJCXXByID(ES_WHYL_TSYXRJJBXX.JCXXID);
+                    return new { Result = EnResultType.Success, Message = "载入成功", Value = new { ES_WHYL_TSYXRJJBXX = ES_WHYL_TSYXRJJBXX, JCXX = jcxx, Photos = GetPhtosByJCXXID(ES_WHYL_TSYXRJJBXX.JCXXID) } };
+                }
+                else
+                {
+                    return new { Result = EnResultType.Failed, Message = "不存在" };
+                }
+            }
+            catch (Exception ex)
+            {
+                LoggerManager.Error("ES_WHYL_TSYXRJJBXXBLL", "载入失败【" + ex.Message + "\r\n" + ex.StackTrace + "】!");
+                return new
+                {
+                    Result = EnResultType.Failed,
+                    Message = "载入失败【" + ex.Message + "\r\n" + ex.StackTrace + "】!"
+                };
+            }
+        }
+    }
+}
