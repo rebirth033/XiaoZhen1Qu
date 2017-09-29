f8c2d39dd653898b82cddfdd41d3e0bba970ad25
rebirth033 <980381266@qq.com>
2017-09-29 18:09:30 +0800

    0929

---
+++ TianWen.XiaoZhen1Qu/BLL/PFCG/PFCG_FZBLBLL.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using NHibernate;
+using TianWen.Framework.Log;
+using TianWen.XiaoZhen1Qu.Entities.Models;
+using TianWen.XiaoZhen1Qu.Interface;
+
+namespace TianWen.XiaoZhen1Qu.BLL
+{
+    public class PFCG_FZBLBLL : BaseBLL, IPFCG_FZBLBLL
+    {
+        public object SavePFCG_FZBLJBXX(JCXX jcxx, PFCG_FZBLJBXX PFCG_FZBLJBXX, List<PHOTOS> photos)
+        {
+            string[] photoNames = photos.Select(x => x.PHOTONAME).ToArray();
+            DirectoryInfo TheFolder = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory + "/Areas/Business/Photos/");
+            FileInfo[] fileinfos = TheFolder.GetFiles();
+            DataTable dt = DAO.Repository.GetDataTable(string.Format("SELECT * FROM PFCG_FZBLJBXX WHERE PFCG_FZBLJBXXID='{0}'", PFCG_FZBLJBXX.PFCG_FZBLJBXXID));
+
+            if (dt.Rows.Count > 0)
+            {
+                using (ITransaction transaction = DAO.BeginTransaction())
+                {
+                    try
+                    {
+                        if (photos.Count > 0)
+                        {
+                            //照片全删全插
+                            List<PHOTOS> old_photos = DAO.GetObjectList<PHOTOS>(string.Format("FROM PHOTOS WHERE JCXXID='{0}'", dt.Rows[0]["JCXXID"])).ToList();
+                            foreach (var obj in old_photos)
+                            {
+                                DAO.Remove(obj);
+                            }
+                        }
+                        foreach (var obj in photos)
+                        {
+                            obj.JCXXID = dt.Rows[0]["JCXXID"].ToString();
+                            DAO.Save(obj);
+                        }
+
+                        if (photos.Count > 0)
+                        {
+                            //删除多余照片文件
+                            foreach (FileInfo fileobj in fileinfos)
+                            {
+                                if (!photoNames.Contains(fileobj.Name))
+                                    File.Delete(AppDomain.CurrentDomain.BaseDirectory + "/Areas/Business/Photos/" + fileobj.Name);
+                            }
+                        }
+
+                        PFCG_FZBLJBXX.JCXXID = dt.Rows[0]["JCXXID"].ToString();
+                        jcxx.JCXXID = dt.Rows[0]["JCXXID"].ToString();
+                        DAO.Update(jcxx);
+                        DAO.Update(PFCG_FZBLJBXX);
+                        transaction.Commit();
+                        return new { Result = EnResultType.Success, Message = "修改成功!", Value = new { JCXXID = jcxx.JCXXID, PFCG_FZBLJBXXID = PFCG_FZBLJBXX.PFCG_FZBLJBXXID } };
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        LoggerManager.Error("PFCG_FZBLJBXXBLL", "保存失败【" + ex.Message + "\r\n" + ex.StackTrace + "】!");
+                        return new
+                        {
+                            Result = EnResultType.Failed,
+                            Message = "保存失败【" + ex.Message + "\r\n" + ex.StackTrace + "】!",
+                            Type = 3
+                        };
+                    }
+                }
+            }
+            else
+            {
+                using (ITransaction transaction = DAO.BeginTransaction())
+                {
+                    try
+                    {
+                        PFCG_FZBLJBXX.JCXXID = jcxx.JCXXID;
+                        DAO.Save(jcxx);
+                        DAO.Save(PFCG_FZBLJBXX);
+                        //照片全删全插
+                        List<PHOTOS> old_photos = DAO.GetObjectList<PHOTOS>(string.Format("FROM PHOTOS WHERE JCXXID='{0}'", PFCG_FZBLJBXX.JCXXID)).ToList();
+                        foreach (var obj in old_photos)
+                        {
+                            DAO.Remove(obj);
+                        }
+                        foreach (var obj in photos)
+                        {
+                            obj.JCXXID = PFCG_FZBLJBXX.JCXXID;
+                            DAO.Save(obj);
+                        }
+                        transaction.Commit();
+                        return new { Result = EnResultType.Success, Message = "新增成功!", Value = new { JCXXID = jcxx.JCXXID, PFCG_FZBLJBXXID = PFCG_FZBLJBXX.PFCG_FZBLJBXXID } };
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        LoggerManager.Error("PFCG_FZBLJBXXBLL", "保存失败【" + ex.Message + "\r\n" + ex.StackTrace + "】!");
+                        return new
+                        {
+                            Result = EnResultType.Failed,
+                            Message = "保存失败【" + ex.Message + "\r\n" + ex.StackTrace + "】!",
+                            Type = 3
+                        };
+                    }
+                }
+            }
+        }
+
+        public object LoadPFCG_FZBLJBXX(string PFCG_FZBLJBXXID)
+        {
+            try
+            {
+                PFCG_FZBLJBXX PFCG_FZBLJBXX = DAO.GetObjectByID<PFCG_FZBLJBXX>(PFCG_FZBLJBXXID);
+                if (PFCG_FZBLJBXX != null)
+                {
+                    JCXX jcxx = GetJCXXByID(PFCG_FZBLJBXX.JCXXID);
+                    return new { Result = EnResultType.Success, Message = "载入成功", Value = new { PFCG_FZBLJBXX = PFCG_FZBLJBXX, JCXX = jcxx, Photos = GetPhtosByJCXXID(PFCG_FZBLJBXX.JCXXID) } };
+                }
+                else
+                {
+                    return new { Result = EnResultType.Failed, Message = "不存在" };
+                }
+            }
+            catch (Exception ex)
+            {
+                LoggerManager.Error("PFCG_FZBLJBXXBLL", "载入失败【" + ex.Message + "\r\n" + ex.StackTrace + "】!");
+                return new
+                {
+                    Result = EnResultType.Failed,
+                    Message = "载入失败【" + ex.Message + "\r\n" + ex.StackTrace + "】!"
+                };
+            }
+        }
+    }
+}
