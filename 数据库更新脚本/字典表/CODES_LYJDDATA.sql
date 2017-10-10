9903b802616260bea9c0e9aa8b79354ae54f42e9
rebirth033 <980381266@qq.com>
2017-10-10 17:30:53 +0800

    1010

---
+++ 数据库更新脚本/字典表/CODES_LYJDDATA.sql
@@ -0,0 +1,43 @@
+prompt Importing table codes_lyjd...
+set feedback off
+set define off
+insert into codes_lyjd (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (1, '出游方式', '跟团游', '1', 1, null);
+
+insert into codes_lyjd (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (2, '出游方式', '自驾游', '2', 2, null);
+
+insert into codes_lyjd (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (3, '出游方式', '自由行', '3', 3, null);
+
+insert into codes_lyjd (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (4, '往返交通_去', '飞机去', '1', 1, null);
+
+insert into codes_lyjd (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (5, '往返交通_去', '高铁去', '2', 2, null);
+
+insert into codes_lyjd (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (6, '往返交通_去', '火车去', '3', 3, null);
+
+insert into codes_lyjd (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (7, '往返交通_去', '汽车去', '4', 4, null);
+
+insert into codes_lyjd (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (8, '往返交通_去', '游轮去', '5', 5, null);
+
+insert into codes_lyjd (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (9, '往返交通_回', '飞机回', '1', 1, null);
+
+insert into codes_lyjd (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (10, '往返交通_回', '高铁回', '2', 2, null);
+
+insert into codes_lyjd (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (11, '往返交通_回', '火车回', '3', 3, null);
+
+insert into codes_lyjd (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (12, '往返交通_回', '汽车回', '4', 4, null);
+
+insert into codes_lyjd (CODEID, TYPENAME, CODENAME, CODEVALUE, CODEORDER, PARENTID)
+values (13, '往返交通_回', '游轮回', '5', 5, null);
+
+prompt Done.
