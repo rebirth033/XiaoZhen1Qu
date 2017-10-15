6513fe759a73a89bf9847edbf67ae9b1ec8fd4f1
rebirth033 <980381266@qq.com>
2017-10-15 18:21:57 +0800

    1015

---
+++ TianWen.XiaoZhen1Qu.Entities/Maps/NLMFY/NLMFY_NCPJGJBXX_Map.cs
@@ -0,0 +1,32 @@
+﻿using FluentNHibernate.Mapping;
+using TianWen.XiaoZhen1Qu.Entities.Models;
+
+namespace TianWen.XiaoZhen1Qu.Entities.Maps
+{
+    public class NLMFY_NCPJGJBXX_Map : ClassMap<NLMFY_NCPJGJBXX>
+    {
+        public NLMFY_NCPJGJBXX_Map()
+        {
+            Table("NLMFY_NCPJGJBXX");
+            #region 属性
+            Id(x => x.NLMFY_NCPJGJBXXID, "NLMFY_NCPJGJBXXID").GeneratedBy.Assigned().CustomType("AnsiString");
+            Map(x => x.JCXXID, "JCXXID");
+            Map(x => x.LB, "LB");
+            Map(x => x.XL, "XL");
+            Map(x => x.QY, "QY");
+            Map(x => x.DD, "DD");
+            Map(x => x.JTDZ, "JTDZ");
+            Map(x => x.BCMS, "BCMS");
+            #endregion
+
+            #region OneToMany
+
+            #endregion
+
+            #region ManyToOne
+
+            #endregion
+
+        }
+    }
+}
--- TianWen.XiaoZhen1Qu.Entities/Models/NLMFY/NLMFY_NCPJGJBXX.cs
+++ TianWen.XiaoZhen1Qu.Entities/Models/NLMFY/NLMFY_NCPJGJBXX.cs
@@ -1,11 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using NHibernate.Mapping.Attributes;
 
-namespace TianWen.XiaoZhen1Qu.Entities.Models.NLMFY
+namespace TianWen.XiaoZhen1Qu.Entities.Models
 {
-    class NLMFY_NCPJGJBXX
+    public class NLMFY_NCPJGJBXX
     {
+        public NLMFY_NCPJGJBXX()
+        {
+            NLMFY_NCPJGJBXXID = Guid.NewGuid().ToString("N");
+        }
+
+        /// <summary>
+        /// 农林牧副渔_农产品加工信息ID
+        /// </summary>
+        [Id]
+        public virtual string NLMFY_NCPJGJBXXID { get; set; }
+
+        /// <summary>
+        /// 基础信息ID
+        /// </summary>
+        [Property]
+        public virtual string JCXXID { get; set; }
+
+        /// <summary>
+        /// 类别
+        /// </summary>
+        [Property]
+        public virtual string LB { get; set; }
+
+        /// <summary>
+        /// 小类
+        /// </summary>
+        [Property]
+        public virtual string XL { get; set; }
+
+        /// <summary>
+        /// 区域
+        /// </summary>
+        [Property]
+        public virtual string QY { get; set; }
+
+        /// <summary>
+        /// 地段
+        /// </summary>
+        [Property]
+        public virtual string DD { get; set; }
+
+        /// <summary>
+        /// 具体地址
+        /// </summary>
+        [Property]
+        public virtual string JTDZ { get; set; }
+
+        /// <summary>
+        /// 补充描述
+        /// </summary>
+        [Property]
+        public virtual string BCMS { get; set; }
     }
 }
