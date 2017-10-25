3fd26ca20f908138b85a4a1a7217e3e7cd26ac1e
rebirth033 <980381266@qq.com>
2017-10-25 17:22:24 +0800

    1025

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/PFCG/PFCG_LPYZ.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/PFCG/PFCG_LPYZ.js
@@ -1,10 +1,18 @@
 ﻿$(document).ready(function () {
 
 });
-
+//礼品类别
+function ValidateLB() {
+    if (!ValidateSelect("LPLB", "LB", "忘记选择类别啦")) return false;
+    if (!ValidateSelect("LPLB", "XL", "忘记选择小类啦")) return false;
+    return true;
+}
 //验证所有
 function ValidateAll() {
-    if (ValidateBT() & ValidateZP() & ValidateLXR() & ValidateLXDH())
+    if (ValidateLB()
+        & ValidateBCMS("BCMS", "忘记填写补充描述啦")
+        & ValidateXXDZ()
+        & ValidateCommon())
         return true;
     else
         return false;
