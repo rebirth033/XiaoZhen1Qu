cce39b5a8e91a4b0c95537297467b3eec267a600
rebirth033 <980381266@qq.com>
2017-10-30 15:23:39 +0800

    1030

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/SY.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/SY.js
@@ -1,3 +1,9 @@
 ﻿$(document).ready(function () {
-    $(".img_head_left").css("margin-left", (document.documentElement.clientWidth - 1200) / 2);
+    $(".div_top_left").css("margin-left", (document.documentElement.clientWidth - 1200) / 2);
+    $(".div_top_right").css("margin-right", (document.documentElement.clientWidth - 1200) / 2);
+    $(".div_head").css("margin-left", (document.documentElement.clientWidth - 1200) / 2);
+    $(".div_head_nav").css("margin-left", (document.documentElement.clientWidth - 1200) / 2);
+    $(".img_head_left").css("margin-left", "20px");
+    $("#li_head_sy").css("background", "#5bc0de").css("color", "#ffffff");
+
 });
