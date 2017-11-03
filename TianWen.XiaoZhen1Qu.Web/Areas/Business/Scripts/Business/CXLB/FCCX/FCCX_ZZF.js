70b9788628b87100fbbfbe9206b2e3777d8cb8f3
rebirth033 <980381266@qq.com>
2017-11-03 11:51:57 +0800

    1103

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/CXLB/FCCX/FCCX_ZZF.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/CXLB/FCCX/FCCX_ZZF.js
@@ -1,7 +1,18 @@
-﻿$(document).ready(function () {
+﻿$(document).ready(function() {
     $(".div_top_left").css("margin-left", (document.documentElement.clientWidth - 1200) / 2);
     $(".div_top_right").css("margin-right", (document.documentElement.clientWidth - 1200) / 2);
-    $(".div_head").css("margin-left", (document.documentElement.clientWidth - 1200) / 2);
-    $(".div_head_nav").css("margin-left", (document.documentElement.clientWidth - 1200) / 2);
+    $(".div_nav").css("margin-left", (document.documentElement.clientWidth - 1200) / 2);
+    $(".div_condition").css("margin-left", (document.documentElement.clientWidth - 1200) / 2);
 
+    $("#li_condition_head_qyzf").css("background-color", "#ffffff");
+    BindConditionLi();
 });
+
+function BindConditionLi() {
+    $(".li_condition_head").bind("click", function () {
+        $(".li_condition_head").each(function() {
+            $(this).css("background-color", "#eeeff1");
+        });
+        $(this).css("background-color", "#ffffff");
+    });
+}
