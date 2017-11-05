610b04ce52d0b9f07db08cf573b4c30fe712f3fe
rebirth033 <980381266@qq.com>
2017-11-05 22:11:34 +0800

    1105

---
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Controllers/XXXX/FCXXController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TianWen.XiaoZhen1Qu.Interface;
+
+namespace TianWen.XiaoZhen1Qu.Web.Areas.Business.Controllers
+{
+    public class FCXXController : BaseController
+    {
+        public IFCXXBLL FCXXBLL { get; set; }
+    }
+}
--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/CXLB/FCCX/FCCX_ZZF.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/CXLB/FCCX/FCCX_ZZF.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
     $(".div_condition").css("margin-left", (document.documentElement.clientWidth - 1200) / 2);
     $(".div_condition_select").css("margin-left", (document.documentElement.clientWidth - 1200) / 2);
     $(".div_body").css("margin-left", (document.documentElement.clientWidth - 1200) / 2);
-
     $("#li_condition_head_qyzf").css("background-color", "#ffffff");
     $(".li_body_head:eq(0)").css("border-bottom", "2px solid #5bc0de").css("color", "#5bc0de").css("font-weight", "700");
     $("#span_fbxx").bind("click", OpenLBXZ);
