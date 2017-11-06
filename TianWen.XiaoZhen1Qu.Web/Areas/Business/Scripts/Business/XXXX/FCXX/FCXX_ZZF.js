8ec97011d623327ee949bdc6c4ac67f41e4486fb
rebirth033 <980381266@qq.com>
2017-11-06 20:56:19 +0800

    1106

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/XXXX/FCXX/FCXX_ZZF.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/XXXX/FCXX/FCXX_ZZF.js
@@ -3,4 +3,19 @@
     $(".div_top_right").css("margin-right", (document.documentElement.clientWidth - 1200) / 2);
     $(".div_nav").css("margin-left", (document.documentElement.clientWidth - 1200) / 2);
     $(".div_body").css("margin-left", (document.documentElement.clientWidth - 1200) / 2);
+    LoadDefault();
 });
+
+function LoadDefault() {
+    
+}
+
+function LoadDTXX() {
+    var map = new BMap.Map("container");//创建地图实例
+    map.centerAndZoom("福州市", 11);//创建点坐标,地图初始化
+    map.enableScrollWheelZoom(true);//允许鼠标滑轮放大缩小 
+    map.enableContinuousZoom(true);//允许惯性拖拽
+    map.addControl(new BMap.NavigationControl({ isOpen: true, anchor: BMAP_ANCHOR_BOTTOM_LEFT }));  //添加默认缩放平移控件,右上角打开
+    map.addControl(new BMap.OverviewMapControl({ isOpen: true, anchor: BMAP_ANCHOR_BOTTOM_RIGHT })); //添加默认缩略地图控件,右下角打开
+    map.addControl(new BMap.MapTypeControl());//添加卫星控件
+}
