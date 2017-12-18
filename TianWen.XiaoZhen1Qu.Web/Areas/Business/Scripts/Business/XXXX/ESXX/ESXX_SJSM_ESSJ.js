d515372c6d6ba6728b5135213d6d044019eceff8
rebirth033 <980381266@qq.com>
2017-12-18 09:50:51 +0800

    1218

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/XXXX/ESXX/ESXX_SJSM_ESSJ.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/XXXX/ESXX/ESXX_SJSM_ESSJ.js
@@ -62,8 +62,9 @@ function LoadJBXX(obj) {
     html += ('<span class="span_body_left_body_right_left">区域：</span>');
     html += ('<span class="span_body_left_body_right_right">' + obj.QY + '-' + obj.DD + '</span>');
     html += ('</p>');
-    html += ('<p class="p_body_left_body_right_lxdh" style="margin-top:78px;">');
-    html += ('<img class="img_body_left_body_right_lxdh" src="' + getRootPath() + '/Areas/Business/Css/images/lxdh.png" />' + obj.LXDH);
+    html += ('<p class="p_body_left_body_right">');
+    html += ('<span class="span_body_left_body_right_left">联系电话：</span>');
+    html += ('<span class="span_body_left_body_right_right span_body_left_body_right_right_lxdh">' + obj.LXDH.substr(0, 7) + '****' + '</span>');
     html += ('</p>');
     html += ('</div>');
     html += ('</div>');
