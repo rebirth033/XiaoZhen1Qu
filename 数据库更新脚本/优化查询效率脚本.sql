3730338c6103c364ae58958e373c362f9dba0aaf
rebirth033 <980381266@qq.com>
2017-12-22 16:58:55 +0800

    1222

--- 数据库更新脚本/优化查询效率脚本.sql
+++ 数据库更新脚本/优化查询效率脚本.sql
@@ -1,2 +1,4 @@
 --小区基本信息字典表索引
-create index ind_codes_xqjbxx_xqmc on codes_xqjbxx(xqmc);
+create index IND_CODES_XQJBXX_XQMC on codes_xqjbxx(XQMC);
+create index IND_CODES_XQJBXX_XQMCPYQKG on codes_xqjbxx(XQMCPYQKG);
+create index IND_CODES_XQJBXX_XQMCPYSZM on codes_xqjbxx(XQMCPYSZM);
