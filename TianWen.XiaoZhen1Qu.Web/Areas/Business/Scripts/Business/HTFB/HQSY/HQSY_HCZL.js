7a3557ae4e941b739bdd234c2fc8f8dd606050ee
rebirth033 <980381266@qq.com>
2018-01-06 12:55:36 +0800

    0106

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/HTFB/HQSY/HQSY_HCZL.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/HTFB/HQSY/HQSY_HCZL.js
@@ -1,19 +1,17 @@
 ﻿$(document).ready(function () {
-    BindClick("LB");
-    BindClick("TCPP");
-    BindClick("GCPP");
     LoadTCYS();
-    LoadGCYS();
-    LoadDuoX("婚车租赁", "TGFW");
+    //LoadGCYS();
+    //LoadDuoX("婚车租赁", "TGFW");
 });
-//加载头车颜色
+//加载车辆颜色
 function LoadTCYS() {
-    var colors = new Array("black:黑", "red:红", "white:白", "rgb(230, 230, 230):银", "rgb(214, 214, 214):灰", "rgb(51, 153, 255):蓝", "rgb(255, 255, 0):黄", "rgb(153, 102, 0):棕");
+    var colors = new Array("black:黑", "white:白", "red:红", "rgb(255, 255, 0):黄", "rgb(51, 153, 255):蓝", "rgb(13, 207, 110):绿", "rgb(255, 102, 0):橙");
     for (var i = 0; i < colors.length; i++) {
         var obj = colors[i].split(':');
         $("#div_tcys_right").append('<div class="div_tcys"><span class="span_tcys_left" style="background-color: ' + obj[0] + '"></span><span class="span_tcys_right">' + obj[1] + '</span></div>');
     }
-    colors = new Array("rgb(13, 207, 110):绿", "rgb(255, 102, 0):橙", "rgb(204, 51, 153):紫", "rgb(243, 238, 170):香槟", "rgb(255, 204, 0):金", "rgb(255, 192, 203):粉红", "其他");
+    alert()
+    colors = new Array("rgb(204, 51, 153):紫", "rgb(255, 204, 0):金", "rgb(230, 230, 230):银", "rgb(214, 214, 214):灰", "rgb(153, 102, 0):棕", "rgb(255, 192, 203):粉红", "其他");
     for (var i = 0; i < colors.length; i++) {
         var obj = colors[i].split(':');
         if (i === colors.length - 1)
@@ -23,227 +21,28 @@ function LoadTCYS() {
     }
     $(".div_tcys").bind("click", ActiveTCYS);
 }
-//加载跟车颜色
-function LoadGCYS() {
-    var colors = new Array("black:黑", "red:红", "white:白", "rgb(230, 230, 230):银", "rgb(214, 214, 214):灰", "rgb(51, 153, 255):蓝", "rgb(255, 255, 0):黄", "rgb(153, 102, 0):棕");
-    for (var i = 0; i < colors.length; i++) {
-        var obj = colors[i].split(':');
-        $("#div_gcys_right").append('<div class="div_gcys"><span class="span_gcys_left" style="background-color: ' + obj[0] + '"></span><span class="span_gcys_right">' + obj[1] + '</span></div>');
-    }
-    colors = new Array("rgb(13, 207, 110):绿", "rgb(255, 102, 0):橙", "rgb(204, 51, 153):紫", "rgb(243, 238, 170):香槟", "rgb(255, 204, 0):金", "rgb(255, 192, 203):粉红", "其他");
-    for (var i = 0; i < colors.length; i++) {
-        var obj = colors[i].split(':');
-        if (i === colors.length - 1)
-            $("#div_gcys_right2").append('<div class="div_gcys"><span class="span_gcys_left" style="border:none;"></span><span class="span_gcys_right">' + obj[0] + '</span></div>');
-        else
-            $("#div_gcys_right2").append('<div class="div_gcys"><span class="span_gcys_left" style="background-color: ' + obj[0] + '"></span><span class="span_gcys_right">' + obj[1] + '</span></div>');
-    }
-    $(".div_gcys").bind("click", ActiveGCYS);
+//获取车辆颜色
+function GetTCYS() {
+    var value = "";
+    $(".div_tcys").each(function () {
+        if ($(this).css("background-color") === "rgb(188, 107, 166)")
+            value = $(this).find(".span_tcys_right")[0].innerHTML;
+    });
+    return value;
 }
-//选择头车颜色
+//设置车辆颜色
+function SetTCYS(tcys) {
+    $(".div_tcys").each(function () {
+        if ($(this).find(".span_tcys_right")[0].innerHTML === tcys)
+            $(this).css("border-color", "#bc6ba6").css("background-color", "#bc6ba6");
+    });
+}
+//选择车辆颜色
 function ActiveTCYS() {
     $(".div_tcys").each(function () {
-        $(this).css("background-color", "#ffffff;");
+        $(this).css("border-color", "#cccccc").css("background-color", "#ffffff");;
     });
-    $(this).css("background-color", "#87B53B");
-}
-//选择跟车颜色
-function ActiveGCYS() {
-    $(".div_gcys").each(function () {
-        $(this).css("background-color", "#ffffff;");
-    });
-    $(this).css("background-color", "#87B53B");
-}
-//加载头车品牌标签
-function LoadTCPP(type) {
-    var arrayObj = new Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
-    var html = "";
-    for (var i = 0; i < arrayObj.length; i++) {
-        html += '<div class="div_bqss_content_bq" id="div' + arrayObj[i] + '"><span class="span_bqss_content_bq" id="span' + arrayObj[i] + '">' + arrayObj[i] + '</span><em class="em_bqss_content_bq" id="em' + arrayObj[i] + '"></em></div>';
-    }
-    $("#div_content_" + type + "bq").html(html);
-    $(".div_bqss_content_bq").bind("click", TCBQActive);
-}
-//头车品牌标签切换
-function TCBQActive() {
-    LoadTCPPMC("婚车品牌", this.id);
-}
-//加载头车品牌名称
-function LoadTCPPMC(JCLX, JCBQ) {
-    $.ajax({
-        type: "POST",
-        url: getRootPath() + "/Business/Common/LoadByCodeValueAndTypeName",
-        dataType: "json",
-        data:
-        {
-            CODEVALUE: JCBQ.split("div")[1],
-            TYPENAME: "婚车品牌",
-            TBName: "CODES_HQSY"
-        },
-        success: function (xml) {
-            if (xml.Result === 1) {
-                var html = "";
-                for (var i = 0; i < xml.list.length; i++) {
-                    html += '<span class="span_mc" onclick="TCPPXZ(\'' + xml.list[i].CODENAME + '\',\'' + xml.list[i].CODEID + '\')">' + xml.list[i].CODENAME + '</span>';
-                }
-                if (xml.list.length === 0)
-                    html += '<span class="span_mc" style=\"width:200px;text-align:left;margin-left:14px;\">该字母下暂无数据</span>';
-                $("#div_content_tcmc").html(html);
-                $("#divTCPP").css("display", "block");
-            }
-        },
-        error: function (XMLHttpRequest, textStatus, errorThrown) { //有错误时的回调函数
-
-        }
-    });
-}
-//选择头车品牌名称
-function TCPPXZ(TCPPMC, TCPPID) {
-    $("#spanTCPP").html(TCPPMC);
-    $("#divTCPP").css("display", "none");
-    ValidateSelect("HCZLTCPP", "TCPP", "忘记选择头车品牌啦");
-}
-//加载跟车品牌标签
-function LoadGCPP(type) {
-    var arrayObj = new Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
-    var html = "";
-    for (var i = 0; i < arrayObj.length; i++) {
-        html += '<div class="div_bqss_content_bq" id="div' + arrayObj[i] + '"><span class="span_bqss_content_bq" id="span' + arrayObj[i] + '">' + arrayObj[i] + '</span><em class="em_bqss_content_bq" id="em' + arrayObj[i] + '"></em></div>';
-    }
-    $("#div_content_" + type + "bq").html(html);
-    $(".div_bqss_content_bq").bind("click", GCBQActive);
-}
-//跟车品牌标签切换
-function GCBQActive() {
-    LoadGCPPMC("婚车品牌", this.id);
-}
-//加载跟车品牌名称
-function LoadGCPPMC(JCLX, JCBQ) {
-    $.ajax({
-        type: "POST",
-        url: getRootPath() + "/Business/Common/LoadByCodeValueAndTypeName",
-        dataType: "json",
-        data:
-        {
-            CODEVALUE: JCBQ.split("div")[1],
-            TYPENAME: "婚车品牌",
-            TBName: "CODES_HQSY"
-        },
-        success: function (xml) {
-            if (xml.Result === 1) {
-                var html = "";
-                for (var i = 0; i < xml.list.length; i++) {
-                    html += '<span class="span_mc" onclick="GCPPXZ(\'' + xml.list[i].CODENAME + '\',\'' + xml.list[i].CODEID + '\')">' + xml.list[i].CODENAME + '</span>';
-                }
-                if (xml.list.length === 0)
-                    html += '<span class="span_mc" style=\"width:200px;text-align:left;margin-left:14px;\">该字母下暂无数据</span>';
-                $("#div_content_gcmc").html(html);
-                $("#divGCPP").css("display", "block");
-            }
-        },
-        error: function (XMLHttpRequest, textStatus, errorThrown) { //有错误时的回调函数
-
-        }
-    });
-}
-//选择跟车品牌名称
-function GCPPXZ(GCPPMC, GCPPID) {
-    $("#spanGCPP").html(GCPPMC);
-    $("#divGCPP").css("display", "none");
-    ValidateSelect("HCZLGCPP", "GCPP", "忘记选择跟车品牌啦");
-}
-//绑定下拉框
-function BindClick(type) {
-    $("#div" + type + "Span").click(function () {
-        if (type === "TCPP") {
-            LoadTCPP("tc");
-            LoadTCPPMC("婚车品牌", "divA");
-        }
-        if (type === "GCPP") {
-            LoadGCPP("gc");
-            LoadGCPPMC("婚车品牌", "divA");
-        }
-        if (type === "XL") {
-            LoadXL();
-        }
-    });
-}
-//选择类别下拉框
-function SelectLB(obj, type, lbid) {
-    $("#span" + type).html(obj.innerHTML);
-    $("#div" + type).css("display", "none");
-    $("#LBID").val(lbid);
-}
-//加载多选
-function LoadDuoX(type, id) {
-    $.ajax({
-        type: "POST",
-        url: getRootPath() + "/Business/Common/LoadCODESByTYPENAME",
-        dataType: "json",
-        data:
-        {
-            TYPENAME: type,
-            TBName: "CODES_HQSY"
-        },
-        success: function (xml) {
-            if (xml.Result === 1) {
-                var html = "<ul class='ulFWPZ'>";
-                for (var i = 0; i < xml.list.length; i++) {
-                    html += "<li class='li" + id + "' onclick='SelectDuoX(this)'><img class='img_" + id + "'/><label style='font-weight:normal;'>" + xml.list[i].CODENAME + "</label></li>";
-                    if (i % 4 === 3 && i !== (xml.list.length - 1)) {
-                        html += "</ul><ul class='ulFWPZ' style='margin-left: 183px'>";
-                    }
-                }
-                if (parseInt(xml.list.length % 4) === 0)
-                    $("#div" + id).css("height", parseInt(xml.list.length / 4) * 45 + "px");
-                else
-                    $("#div" + id).css("height", (parseInt(xml.list.length / 4) + 1) * 45 + "px");
-                html += "</ul>";
-                $("#div" + id + "Text").html(html);
-                $(".img_" + id).attr("src", getRootPath() + "/Areas/Business/Css/images/check_gray.png");
-                LoadJBXX();
-            }
-        },
-        error: function (XMLHttpRequest, textStatus, errorThrown) { //有错误时的回调函数
-
-        }
-    });
-}
-//获取头车颜色
-function GetTCYS(type) {
-    var ys = "";
-    $("#div" + type).find(".div_tcys").each(function () {
-        if ($(this).css("background-color") === "rgb(135, 181, 59)") {
-            ys = $(this).find(".span_tcys_right")[0].innerHTML;
-        }
-    });
-    return ys;
-}
-//获取跟车颜色
-function GetGCYS(type) {
-    var ys = "";
-    $("#div" + type).find(".div_gcys").each(function () {
-        if ($(this).css("background-color") === "rgb(135, 181, 59)") {
-            ys = $(this).find(".span_gcys_right")[0].innerHTML;
-        }
-    });
-    return ys;
-}
-//设置头车颜色
-function SetTCYS(type, ys) {
-    $("#div" + type).find(".div_tcys").each(function () {
-        if ($(this).find(".span_tcys_right")[0].innerHTML === ys) {
-            $(this).css("background-color", "#87B53B");
-        }
-    });
-}
-//设置跟车颜色
-function SetGCYS(type, ys) {
-    $("#div" + type).find(".div_gcys").each(function () {
-        if ($(this).find(".span_gcys_right")[0].innerHTML === ys) {
-            $(this).css("background-color", "#87B53B");
-        }
-    });
+    $(this).css("border-color", "#bc6ba6").css("background-color", "#bc6ba6");
 }
 //加载婚庆摄影_婚车租赁基本信息
 function LoadJBXX() {
@@ -272,8 +71,8 @@ function LoadJBXX() {
                     SetDX("TCCZ", xml.Value.HQSY_HCZLJBXX.TCCZ);
                 if (xml.Value.HQSY_HCZLJBXX.MFTGCH !== null)
                     SetDX("MFTGCH", xml.Value.HQSY_HCZLJBXX.MFTGCH);
-                if (xml.Value.HQSY_HCZLJBXX.TCYS !== null)
-                    SetTCYS("TCYS", xml.Value.HQSY_HCZLJBXX.TCYS);
+                if (xml.Value.HQSY_HCZLJBXX.tcys !== null)
+                    Settcys("tcys", xml.Value.HQSY_HCZLJBXX.tcys);
                 if (xml.Value.HQSY_HCZLJBXX.GCYS !== null)
                     SetGCYS("GCYS", xml.Value.HQSY_HCZLJBXX.GCYS);
             }
@@ -296,7 +95,7 @@ function FB() {
     obj = jsonObj.AddJson(obj, "DD", "'" + $("#spanDD").html() + "'");
     obj = jsonObj.AddJson(obj, "TCCZ", "'" + GetDX("TCCZ") + "'");
     obj = jsonObj.AddJson(obj, "MFTGCH", "'" + GetDX("MFTGCH") + "'");
-    obj = jsonObj.AddJson(obj, "TCYS", "'" + GetTCYS("TCYS") + "'");
+    obj = jsonObj.AddJson(obj, "tcys", "'" + Gettcys("tcys") + "'");
     obj = jsonObj.AddJson(obj, "GCYS", "'" + GetGCYS("GCYS") + "'");
 
     if (getUrlParam("ID") !== null)
