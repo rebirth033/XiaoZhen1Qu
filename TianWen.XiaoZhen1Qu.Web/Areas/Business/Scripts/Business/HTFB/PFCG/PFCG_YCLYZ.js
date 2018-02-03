4af5d9230dda76c370b4916886be916f0eb32f7e
rebirth033 <980381266@qq.com>
2018-02-03 23:07:41 +0800

    0203

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/HTFB/PFCG/PFCG_YCLYZ.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/HTFB/PFCG/PFCG_YCLYZ.js
@@ -12,6 +12,7 @@ function ValidateLB() {
 //验证所有
 function ValidateAll() {
     if (ValidateLB()
+        & ValidateCheck("FWFW", "忘记选择服务范围啦")
         & ValidateBCMS("BCMS", "忘记填写详情描述啦")
         & ValidateXXDZ()
         & ValidateCommon())
--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/HTFB/PFCG/PFCG_YXYZ.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/HTFB/PFCG/PFCG_YXYZ.js
@@ -4,6 +4,7 @@
 //验证所有
 function ValidateAll() {
     if (ValidateBCMS("BCMS", "忘记填写详情描述啦")
+        & ValidateCheck("FWFW", "忘记选择服务范围啦")
         & ValidateXXDZ()
         & ValidateCommon())
         return true;
