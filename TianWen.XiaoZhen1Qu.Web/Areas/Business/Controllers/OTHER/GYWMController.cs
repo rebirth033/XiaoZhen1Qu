c22fb79380ee32569b626a64509568ce210e24b6
rebirth033 <980381266@qq.com>
2018-02-04 17:28:15 +0800

    0204

---
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Controllers/OTHER/GYWMController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using TianWen.XiaoZhen1Qu.Web.Areas.Business.Common;
+using TianWen.XiaoZhen1Qu.Interface;
+
+namespace TianWen.XiaoZhen1Qu.Web.Areas.Business.Controllers
+{
+    public class GYWMController : BaseController
+    {
+        //public IGYWMBLL GYWMBLL { get; set; }
+
+        public ActionResult GYWM()
+        {
+            ViewData["XZQ"] = Session["XZQ"];
+            ViewData["YHM"] = Session["YHM"];
+            return View();
+        }
+
+        //public JsonResult LoadDL()
+        //{
+        //    return Json(GYWMBLL.LoadDL());
+        //}
+    }
+}
