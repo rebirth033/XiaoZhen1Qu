ea7463103c63d681a88fea8868ef7120aaf5764e
rebirth033 <980381266@qq.com>
2018-02-07 22:16:16 +0800

    0207

--- TianWen.XiaoZhen1Qu.Entities/Maps/CODES/CODES_SY_ML_Map.cs
+++ TianWen.XiaoZhen1Qu.Entities/Maps/CODES/CODES_SY_ML_Map.cs
@@ -20,7 +20,7 @@ namespace TianWen.XiaoZhen1Qu.Entities.Maps.CODES
             Map(x => x.PARENTID, "PARENTID");
             Map(x => x.TYPE, "TYPE");
             Map(x => x.TYPENAME, "TYPENAME");
-            Map(x => x.CONDITION, "CONDITION");
+            Map(x => x.CONDITION, "CONDITIONS");
             Map(x => x.ISHOT, "ISHOT");
             Map(x => x.TYPESHOWNAME, "TYPESHOWNAME");
             Map(x => x.LBURL, "LBURL");
