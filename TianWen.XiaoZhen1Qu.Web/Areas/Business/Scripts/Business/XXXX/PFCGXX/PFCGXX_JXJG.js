d0850b8705414ed0984c45baca30137476559ee7
rebirth033 <980381266@qq.com>
2018-03-01 11:24:58 +0800

    0301

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/XXXX/PFCGXX/PFCGXX_JXJG.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/XXXX/PFCGXX/PFCGXX_JXJG.js
@@ -43,7 +43,7 @@ function LoadJBXX(obj) {
     html += ('</p>');
     html += ('<p class="p_body_left_body_right">');
     html += ('<span class="span_body_left_body_right_left">联系人：</span>');
-    html += ('<span class="span_body_left_body_right_right">' + obj.LXR + '</span>');
+    html += ('<span class="span_body_left_body_right_right">' + ValidateNull(obj.LXR) + '</span>');
     html += ('</p>');
     html += ('<p class="p_body_left_body_right">');
     html += ('<span class="span_body_left_body_right_left">商家地址：</span>');
