2f16cbf0cfefadb533fbc3c41b9fd4ccbfd0322f
rebirth033 <980381266@qq.com>
2018-03-15 18:14:14 +0800

    0315

---
+++ android_app/app/src/main/java/com/example/administrator/infotownletsecond/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.administrator.infotownletsecond;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class LoginActivity extends AppCompatActivity  {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registor);
+    }
+}
+
