9464108e25d1f25240c4adc32aef22a36cbc1db6
rebirth033 <980381266@qq.com>
2018-08-13 11:32:35 +0800

    0813

---
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/HTFB/SWFW/SWFW_GGCM.js
@@ -0,0 +1,102 @@
+﻿$(document).ready(function () {
+    LoadDuoX("广告传媒类别", "LB");
+});
+//加载多选
+function LoadDuoX(type, id) {
+    $.ajax({
+        type: "POST",
+        url: getRootPath() + "/Common/LoadCODESByTYPENAME",
+        dataType: "json",
+        data:
+        {
+            TYPENAME: type,
+            TBName: "CODES_SWFW"
+        },
+        success: function (xml) {
+            if (xml.Result === 1) {
+                var html = "<ul class='ulFWPZ'>";
+                for (var i = 0; i < xml.list.length; i++) {
+                    html += "<li class='li" + id + "' style='width:170px;' onclick='SelectDuoX(this)'><img class='img_" + id + "'/><label style='font-weight:normal;'>" + xml.list[i].CODENAME + "</label></li>";
+                    if (i % 4 === 3 && i !== xml.list.length - 1) {
+                        html += "</ul><ul class='ulFWPZ' style='margin-left: 173px'>";
+                    }
+                }
+                if (parseInt(xml.list.length % 4) === 0)
+                    $("#div" + id).css("height", parseInt(xml.list.length / 4) * 45 + "px");
+                else
+                    $("#div" + id).css("height", (parseInt(xml.list.length / 4) + 1) * 45 + "px");
+                html += "</ul>";
+                $("#div" + id + "Text").html(html);
+                $(".img_" + id).attr("src", getRootPath() + "/Areas/Business/Css/images/check_gray.png");
+                $(".li" + id).bind("click", function () { ValidateCheck(id, "忘记选择类别啦"); });
+                LoadFWFW();
+            }
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) { //有错误时的回调函数
+
+        }
+    });
+}
+//加载商务服务_广告传媒基本信息
+function LoadJBXX() {
+    $.ajax({
+        type: "POST",
+        url: getRootPath() + "/SWFW/LoadSWFW_GGCMJBXX",
+        dataType: "json",
+        data:
+        {
+            ID: getUrlParam("ID")
+        },
+        success: function (xml) {
+            if (xml.Result === 1) {
+                var jsonObj = new JsonDB("myTabContent");
+                jsonObj.DisplayFromJson("myTabContent", xml.Value.SWFW_GGCMJBXX);
+                jsonObj.DisplayFromJson("myTabContent", xml.Value.JCXX);
+                $("#ID").val(xml.Value.SWFW_GGCMJBXX.ID);
+                //设置编辑器的内容
+                ue.ready(function () { ue.setContent(xml.Value.BCMSString); });
+                $("#spanLB").html(xml.Value.SWFW_GGCMJBXX.LB);
+                $("#spanQY").html(xml.Value.SWFW_GGCMJBXX.QY);
+                $("#spanDD").html(xml.Value.SWFW_GGCMJBXX.DD);
+                LoadPhotos(xml.Value.Photos);
+            }
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) { //有错误时的回调函数
+
+        }
+    });
+}
+//发布
+function FB() {
+    if (ValidateAll() === false) return;
+    var jsonObj = new JsonDB("myTabContent");
+    var obj = jsonObj.GetJsonObject();
+    //手动添加如下字段
+    obj = jsonObj.AddJson(obj, "LB", "'" + $("#spanLB").html() + "'");
+    obj = jsonObj.AddJson(obj, "QY", "'" + $("#spanQY").html() + "'");
+    obj = jsonObj.AddJson(obj, "DD", "'" + $("#spanDD").html() + "'");
+    obj = jsonObj.AddJson(obj, "LBID", "'" + getUrlParam("CLICKID") + "'");
+
+    if (getUrlParam("ID") !== null)
+        obj = jsonObj.AddJson(obj, "ID", "'" + getUrlParam("ID") + "'");
+
+    $.ajax({
+        type: "POST",
+        url: getRootPath() + "/SWFW/FBSWFW_GGCMJBXX",
+        dataType: "json",
+        data:
+        {
+            Json: jsonObj.JsonToString(obj),
+            BCMS: ue.getContent(),
+            FWZP: GetPhotoUrls()
+        },
+        success: function (xml) {
+            if (xml.Result === 1) {
+                window.location.href = getRootPath() + "/FBCG/FBCG?LBID=" + getUrlParam("CLICKID") + "&ID=" + xml.Value.ID + "&JCXXID=" + xml.Value.JCXXID;
+            }
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) { //有错误时的回调函数
+
+        }
+    });
+}
