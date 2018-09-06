91cd04bae15ee7add1abf5a54bfe386f72148714
rebirth033 <980381266@qq.com>
2018-09-06 16:47:02 +0800

    0906

--- TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/XXXX/ZXJCXX/ZXJCXX_FWGZ.js
+++ TianWen.XiaoZhen1Qu.Web/Areas/Business/Scripts/Business/XXXX/ZXJCXX/ZXJCXX_FWGZ.js
@@ -81,7 +81,7 @@ function LoadXQ(obj, BCMSString) {
     html += ('<ul class="ul_body_left_body_xq_xx">');
     for (var i = 0; i < obj.PHOTOS.length; i++) {
         html += ('<li class="li_body_left_body_xq_xx">');
-        html += ('<img id="img_body_left_body_xq_xx' + i + '" onload="DrawImage(this,405,350)" onclick="ImgShow(this)" class="img_body_left_body_xq_xx" src="' + getRootPath() + "/Areas/Business/Photos/" + obj.YHID + "/" + obj.PHOTOS[i].PHOTONAME + "?j=" + Math.random() + '" />');
+        html += ('<img id="img_body_left_body_xq_xx' + i + '" onload="DrawImage(this,403,348)" onclick="ImgShow(this)" class="img_body_left_body_xq_xx" src="' + getRootPath() + "/Areas/Business/Photos/" + obj.YHID + "/" + obj.PHOTOS[i].PHOTONAME + "?j=" + Math.random() + '" />');
         html += ('</li>');
     }
     html += ('</ul>');
